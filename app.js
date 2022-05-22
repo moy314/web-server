a2ace6f82970ee97ba5ca14a751a2b6eba4ea644
moy314 <moy5912@gmail.com>
2022-05-22 17:19:47 -0500

    2: agregando rutas

--- app.js
+++ app.js
@@ -1,13 +1,24 @@
 const express = require('express')
 const app = express()
 
-app.get('/hola', function (req, res) {
-  res.send('hola mundo ');
-})
+//middlreware servir contenido estático
 
-app.get('/', function (req, res) {
-    res.send('pagina inicial');
+app.use(express.static('public'));
+
+
+
+// app.get('/hola', function (req, res) {
+//   res.send('hola mundo ');
+// })
+
+app.get('/generic', function (req, res) {
+    res.sendFile(__dirname + '/public/generic.html');
   })
+
+  app.get('/elements', function (req, res) {
+    res.sendFile(__dirname + '/public/elements.html');
+  })
+
 
   app.get('*', function (req, res) {
     res.send('404 pagina no encontrada ');
