9298df0b255792c5f7b7fbeed77c85bb3f4a210b
moy314 <moy5912@gmail.com>
2022-05-23 16:45:13 -0500

    4: agregando partials

--- app.js
+++ app.js
@@ -1,22 +1,51 @@
 const express = require('express')
 const app = express()
+var hbs = require('hbs');
 
 //middlreware servir contenido estático
 
 app.use(express.static('public'));
+app.set('view engine', 'hbs');
 
 
 
+
+// handlebars
+hbs.registerPartials(__dirname + '/views/partials', function (err) {console.log('error',err)});
+
+
+// app.set('view engine', 'html');
+// app.engine('html', require('hbs').__express);
 // app.get('/hola', function (req, res) {
 //   res.send('hola mundo ');
 // })
 
-app.get('/generic', function (req, res) {
-    res.sendFile(__dirname + '/public/generic.html');
+app.get('/', function (req, res) {
+    res.render('home',{
+
+      nombre:"moises sanchez",
+      titulo:"curso node"
+    });
   })
 
+
+  app.get('/generic', function (req, res) {
+    res.render('generic',{
+
+      nombre:"moises sanchez",
+      titulo:"curso node"
+    });
+  })
+
+
+
+
   app.get('/elements', function (req, res) {
-    res.sendFile(__dirname + '/public/elements.html');
+    res.render('elements',{
+
+      nombre:"moises sanchez",
+      titulo:"curso node"
+    });
   })
 
 
